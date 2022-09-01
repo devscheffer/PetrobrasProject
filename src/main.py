44a87222d0aac6ffeb5a0d02fa1faebc77186794
gerson.scheffer <gerson.scheffer@poatek.com>
2022-09-01 14:13:08 -0300

    extract

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 date_download_lst = [{"start":"2022-08-07","end":"2022-08-13"}]
 
-for i in extract(date_download_lst):
+e =extract(date_download_lst)
+for i in e:
     data =i
     print(data.get("name"))
     print(pd.read_csv(data.get("path")).head(2))
